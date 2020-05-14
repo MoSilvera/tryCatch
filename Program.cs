26adbd3cc7513e846de01cada9e47097bc77f576
Mo Silvera <mo.leslie.silvera@gmail.com>
2020-05-14 11:49:22 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
